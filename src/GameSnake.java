d740657f37230598af51b0dc5356753e57a89b37
denny7794 <denny7794@gmail.com>
2016-10-11 11:37:28 +0400

    Константы и переменные

--- src/GameSnake.java
+++ src/GameSnake.java
@@ -3,5 +3,45 @@
  * Created by Denny7794 on 11.10.2016.
  */
 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+
 public class GameSnake {
+
+    final String TITLE_OF_PROGRAM = "Classic Game Snake";
+    final String GAME_OVER_MSG = "GAME OVER";
+    final int POINT_RADIUS = 20; // in pix
+    final int FIELD_WIDTH = 30; // in point
+    final int FIELD_HEIGHT = 20;
+    final int FIELD_DX = 6;
+    final int FIELD_DY = 28;
+    final int START_LOCATION = 200;
+    final int START_SNAKE_SIZE = 6;
+    final int START_SNAKE_X = 10;
+    final int START_SNAKE_Y = 10;
+    final int SHOW_DELAY = 150;
+    final int LEFT = 37;
+    final int UP = 38;
+    final int RIGHT = 39;
+    final int DOWN = 40;
+    final int START_DIRECTION = RIGHT;
+    final Color DEFAULT_COLOR = Color.black;
+    final Color FOOD_COLOR = Color.green;
+    final Color POISON_COLOR = Color.red;
+    Snake snake;
+    Food food;
+    JFrame frame;
+    Canvas canvas;
+    Random random = new Random();
+    boolean gameOver = false;
+
+    public static void main(String[] args) {
+        new GameSnake().go();
+    }
+
+    void go() {
+
+    }
 }
